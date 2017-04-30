9ae0d2c3a3180fd72975ad8d27918ae6fbca88e9
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-04-30 08:39:14 -0700

    Added an init command.

--- src/index.js
+++ src/index.js
@@ -6,5 +6,8 @@ import project from '../package';
 stm.version(project.version);
 
 // Commands for STM
+stm.command('init')
+    .description('Setup a project managed by STM.')
+    .action(() => console.log('hello'))
 
 stm.parse(process.argv);
