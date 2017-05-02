a6baa97a1ca0d54b70ab830f8100701a2a1015d4
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-05-02 03:25:25 -0700

    Added stripping of indentation for nicer indentation and add help output with a banner when there is no arguments.

--- src/index.js
+++ src/index.js
@@ -2,21 +2,26 @@
 
 import stm from 'commander';
 import indent from 'indent-string';
+import unindent from 'strip-indent';
 import project from '../package';
 
 stm
-    .version(project.version)
-    .help((text) => {
-        console.log(indent(`
-
-STM is a project management system.
-        `, 4));
-        return indent(text, 2);
-    });
+    .version(project.version);
 
 // Commands for STM
 stm.command('init')
     .description('Setup a project managed by STM.')
-    .action(() => console.log('hello'))
+    .action(() => console.log('hello'));
 
 stm.parse(process.argv);
+
+
+if (stm.args.length === 0) {
+    stm.help((text) => {
+        console.log();
+        console.log(indent(unindent(`
+            STM is a project management system.
+        `), 4));
+        return text;
+    });
+}
