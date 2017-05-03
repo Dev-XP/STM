a0fb89a4eb15f829505c6c2b7ad19d2bbd633ae2
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-05-03 05:51:48 -0700

    Converted the commander program into an observable.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,48 @@
 #! /usr/bin/env node
 
+import _ from 'lodash';
 import stm from 'commander';
 import indent from 'indent-string';
 import unindent from 'strip-indent';
+import { Observable } from 'rxjs';
 import project from '../package';
 
-stm
-    .version(project.version);
+const services = [
+    {
+        name: 'init',
+        description: 'Setup a project managed by STM.',
+    },
+];
 
-// Commands for STM
-stm.command('init')
-    .description('Setup a project managed by STM.')
-    .action(() => console.log('hello'));
+const programObservable = Observable.create((observer) => {
+    stm.version(project.version);
+    services.map(s => stm
+        .command(s.name)
+        .description(s.description)
+        .action((command, options) => {
+            observer.next({
+                command: command.name(),
+                options,
+            });
+            observer.complete();
+        })
+    );
+    stm.parse(process.argv);
 
-stm.parse(process.argv);
+    observer.next({})
+    observer.complete();
+});
 
+const help$ = programObservable
+    .filter(_.isEmpty)
+    .do(() => stm
+        .help((text) => {
+            console.log();
+            console.log(indent(unindent(`
+                STM is a project management system.
+            `), 4));
+            return text;
+        })
+    );
 
-if (stm.args.length === 0) {
-    stm.help((text) => {
-        console.log();
-        console.log(indent(unindent(`
-            STM is a project management system.
-        `), 4));
-        return text;
-    });
-}
+help$.subscribe(console.log);
