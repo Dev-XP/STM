ca121f4b98ecb29165e795df31b32eee69f04069
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-05-07 08:15:14 -0700

    Added sub-command detection.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 #! /usr/bin/env node
 
+import _ from 'lodash';
 import { Observable } from 'rxjs';
 import minimist from 'minimist';
 import log from './log';
@@ -9,4 +10,11 @@ Observable
     .do(log('raw args'))
     .map(minimist)
     .do(log('parsed args'))
+    .map(args => ({
+        command: args._[0] || '',
+        params: _.extend(args, {
+            _: args._.slice(1),
+        }),
+    }))
+    .do(log('sub-command'))
     .subscribe(log('result'));
