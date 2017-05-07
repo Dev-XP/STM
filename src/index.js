bcfd944579edba9f67ffc616529df0c38c92c09d
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-05-07 08:09:28 -0700

    Added minimist parsing.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 #! /usr/bin/env node
 
 import { Observable } from 'rxjs';
+import minimist from 'minimist';
+import log from './log';
 
 Observable
     .of(process.argv.slice(2))
-    .do(console.log)
-    .subscribe(console.log);
+    .do(log('raw args'))
+    .map(minimist)
+    .do(log('parsed args'))
+    .subscribe(log('result'));
