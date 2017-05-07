8969a353007199723a1035188df46e099a036b66
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-05-07 08:00:20 -0700

    Starting with the new custom CLI processing system.

--- src/index.js
+++ src/index.js
@@ -1,55 +1,8 @@
 #! /usr/bin/env node
 
-import _ from 'lodash';
-import stm from 'commander';
-import indent from 'indent-string';
-import unindent from 'strip-indent';
 import { Observable } from 'rxjs';
-import project from '../package';
 
-const services = [
-    {
-        name: 'init',
-        description: 'Setup a project managed by STM.',
-    },
-];
-
-const programObservable = Observable.create((observer) => {
-    stm.version(project.version);
-    services.map(s => stm
-        .command(s.name)
-        .description(s.description)
-        .action((command, options) => {
-            observer.next({
-                command: command.name(),
-                options: command.args,
-            });
-            observer.complete();
-        })
-    );
-    stm.parse(process.argv);
-
-    observer.next({})
-    observer.complete();
-});
-
-const help$ = programObservable
-    .filter(_.isEmpty)
-    .do(() => stm
-        .help((text) => {
-            console.log();
-            console.log(indent(unindent(`
-                STM is a project management system.
-            `), 4));
-            return text;
-        })
-    );
-
-const services$ = programObservable
-    .filter(_.negate(_.isEmpty))
-    .do(s => console.log(`Running ${s.command} service.`));
-
-Observable.merge(
-    help$,
-    services$,
-).subscribe();
+Observable
+    .of(process.argv.slice(2))
+    .do(console.log)
+    .subscribe(console.log);
