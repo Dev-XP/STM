5502210a7277fd6f85bfd1ae086c90b5f43d3941
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-05-08 16:30:33 -0700

    Initial setup of Request Validation.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 #! /usr/bin/env node
 
 import _ from 'lodash';
-import { Observable } from 'rxjs';
 import minimist from 'minimist';
+import { Observable, Subject } from 'rx';
 import log from './log';
 
+const isValidRequest = spec => () => true;
+
+const validateSpec = subCommandSpec => stream => stream
+    .filter(isValidRequest(subCommandSpec));
+
 const parseArgs = stream => stream.map(minimist);
+
 const discoverSubCommand = stream => stream
     .map(args => ({
         command: args._[0] || '',
@@ -14,13 +20,21 @@ const discoverSubCommand = stream => stream
         }),
     }));
 
+const validateRequest = stream => stream
+    .mergeMap(params => Observable
+        .of(params)
+        .let(validateSpec(params))
+    );
+
 const commandParser = (programSpecs = {}) =>
     processArgs => Observable
         .of(processArgs)
         .let(parseArgs)
-        .do(log('parsed args'))
+        .do(log('Parsed Args'))
         .let(discoverSubCommand)
-        .do(log('sub-command'));
+        .do(log('Sub-Command'))
+        .let(validateRequest)
+        .do(log('Validated Request'));
 
 Observable
     .of(process.argv.slice(2))
