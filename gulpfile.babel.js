c507795f6207c5cf9084d0e1304733bbabd31afe
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-05-09 16:46:08 -0700

    Added the test task to when a source file changes.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,5 +6,5 @@ gulp.task('default', ['build', 'test']);
 gulp.task('build', run('npm run build'));
 gulp.task('test', run('npm run test'));
 
-gulp.watch('src/**/*', ['build']);
+gulp.watch('src/**/*', ['build','test']);
 gulp.watch('test/**/*', ['test']);
