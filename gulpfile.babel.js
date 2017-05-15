4ac2c04a3465109b2fbaa1684df5de2fb28da3af
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-05-15 15:02:06 -0700

    Updated the command running and sequencing.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,10 +1,19 @@
 import gulp from 'gulp';
-import run from 'gulp-run-command';
+import run from 'gulp-shell';
+import seq from 'gulp-sequence';
 
-gulp.task('default', ['build', 'test']);
+gulp.task('default', seq('build', 'test'));
 
-gulp.task('build', run('npm run build'));
-gulp.task('test', run('npm run test'));
+gulp.task('build', () => {
+    return gulp
+        .src('test/*.js')
+        .pipe(run(['npm run build']));
+});
+gulp.task('test', () => {
+    return gulp
+        .src('test/*.js')
+        .pipe(run(['cross-env NODE_ENV=test babel-node <%= file.path %>']));
+});
 
-gulp.watch('src/**/*', ['build','test']);
+gulp.watch('src/**/*', seq('build', 'test'));
 gulp.watch('test/**/*', ['test']);
