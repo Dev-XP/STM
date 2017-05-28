6e644955fd54074fdff5a78aea7af6c0d3f47287
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-05-28 09:05:25 -0700

    Added a custom assertion for commands.

--- test/command-parser.js
+++ test/command-parser.js
@@ -1,7 +1,23 @@
+import _ from 'lodash';
 import { Observable } from 'rxjs';
-import 'should';
+import should from 'should';
 import describe from 'rxt';
 import commandParser from '../src/modules/command-parser';
+
+should.use(function(s, Assertion) {
+    Assertion.add('command', function(command, parameters = {}) {
+        const parameterStringify = p => _.map(_.toPairs(p), ([k, v]) => `[${k}:${v}]`).join(', ');
+        const commandText = c => _.isEmpty(c) ? 'no command' : `the command [${c}]`;
+        const paramText = p => _.isEmpty(p) ? ' with no parameters' : ` with parameters ${parameterStringify(p)}`;
+        this.params = {
+            operator: 'to actually be',
+            actual: commandText(this.obj.command) + paramText(this.obj.params),
+            expected: commandText(command) + paramText(parameters),
+        };
+        s(this.obj.command).be.equal(command);
+        s(this.obj.params).be.deepEqual(parameters);
+    });
+});
 
 describe('Command Parser', (it) => {
     it('should recognize no command at all', ex => ex
@@ -10,9 +26,6 @@ describe('Command Parser', (it) => {
             .of(command)
             .mergeMap(commandParser())
         )
-        .then(result => result.should.be.deepEqual({
-            command: '',
-            params: {},
-        }))
+        .then(result => result.should.be.command(''))
     );
 });
